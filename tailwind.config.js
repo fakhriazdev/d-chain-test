1329a410085970a25e9a9f4e0102ecb42c14a604
Muhammad Fakhri Aziz <fakhriazdev@gmail.com>
2023-12-09 00:45:28 +0700

    Initial project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontSize: {
+      title: '28px',
+      subtitle: '25px',
+    },
+    extend: {
+      colors: {
+        orange: '#f47321',
+        red: '#f34646',
+        yellow: '#FFF490',
+        green: '#66B56E',
+        blue: '#5BB0FF',
+        darkgray: '#616161',
+        gray: '#757575',
+        lightgray: '#acacac',
+      },
+    },
+  },
+  plugins: [],
+};
