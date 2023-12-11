f39b6da95dc90b081e01063cd9266d289b8783f4
ragil indra <ragilindra9@gmail.com>
2023-12-11 15:43:03 +0700

    [ADD] Auth Layout

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,5 +1,18 @@
-export default function AuthLayout() {
+import loginUser from "../assets/images/LoginUser.png";
+
+export default function AuthLayout(props) {
+    const {children} = props;
   return (
-    <div>AuthLayout</div>
+    <div className="flex h-screen bg-orange justify-end">
+    <img src={loginUser} alt="" className="absolute w-2/4 left-20" />
+    <div className="flex bg-white w-2/3 rounded-l-2xl justify-end">
+      <div className="flex flex-col w-4/5 items-center">
+        <p className="mt-10 text-center text-logo">
+          🔗D-<span className="text-orange font-bold">Autochain</span>
+        </p>
+        {children}
+      </div>
+    </div>
+  </div>
   )
 }
