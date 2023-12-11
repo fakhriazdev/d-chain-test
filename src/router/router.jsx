63efc7c581045552d7fe4a0072da0ef6ce67b3c0
ragil indra <ragilindra9@gmail.com>
2023-12-11 15:16:21 +0700

    [ADD] Feature Forget Password

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Auth/Login";
+
 import Company from "../pages/BackOffice/Company"
 import ListCompany from "../pages/BackOffice/Company/components/ListCompany.jsx";
 import FormCompany from "../pages/BackOffice/Company/components/FormCompany.jsx";
 import EditCompany from "../pages/BackOffice/Company/components/EditCompany.jsx";
 import ListPartnership from "../pages/BackOffice/Partnership/components/ListPartnership.jsx";
 import Partnership from "../pages/BackOffice/Partnership/index.jsx";
+
+import ForgetPassword from "../pages/Auth/ForgetPassword";
+
 
 const setupRouter = () =>
   createBrowserRouter([
@@ -50,8 +54,10 @@ const setupRouter = () =>
             },
           ]
         },
-
-
+        {
+          path: "forget",
+          element: <ForgetPassword />,
+        },
       ],
     },
   ]);
