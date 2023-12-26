56147a3c21440dfff4bf821cdf302f750c6874a8
ragil indra <ragilindra9@gmail.com>
2023-12-26 21:36:12 +0700

    [FIXED] View & Edit Profile

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Auth/Login";
-import ForgetPassword from "../pages/Auth/ForgetPassword";
 import OtpPage from "../pages/Auth/OTP";
 import LoginBackOffice from "../pages/Auth/Login/loginBackOffice";
 import Dashboard from "../pages/Dashboard";
@@ -82,7 +81,7 @@ const setupRouter = () =>
           element: <Profile/>,
           children:[
             {
-              path: "superuser/:id",
+              path: "superuser/:companyId",
               element: <SuperUserProfile/>
             },
           ]
