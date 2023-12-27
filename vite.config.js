808800096f3e3df8648b4c286a03d152f1a277d7
ragil indra <ragilindra9@gmail.com>
2023-12-27 21:19:20 +0700

    [FIXING] Proxy

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,4 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8081",
-        changeOrigin: true,
-        rewrite: (path) => path.replace("^/api/", ""),
-      },
-    },
-  },
 });
